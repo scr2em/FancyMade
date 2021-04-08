9c84acb58a19c5c34196e0fc74f4d3a49a1f27a0
Ahmed-bahaa77 <79056295+Ahmed-bahaa77@users.noreply.github.com>
2021-04-08 08:52:52 +0200

    inbox+model-no onlinestatus-no unread messages+potential for seperate card shared widget-no empty inbox+chat detail

--- lib/ui/homeScreen/Home_Widget.dart
+++ lib/ui/homeScreen/Home_Widget.dart
@@ -1,6 +1,8 @@
+import 'package:finalproject/ui/inbox/inboxscreen.dart';
 import "package:flutter/material.dart";
 import "../../sharedWidgets/CustomAppBar.dart";
 import "../../sharedWidgets/CustomBottomBar.dart";
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,12 +11,14 @@ class HomeScreen extends StatelessWidget {
         body: Center(
           child: ElevatedButton(
             onPressed: () {
-              // Navigate back to first screen when tapped.
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ChatPage()),
+              );
             },
             child: Text('hommee'),
           ),
         ),
-        bottomNavigationBar: CustomBottomBar()
-    );
+        bottomNavigationBar: CustomBottomBar());
   }
 }
