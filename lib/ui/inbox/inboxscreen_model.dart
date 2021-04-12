d889a1bb0afb6519b16653056768451c3908d2b1
Ahmed-bahaa77 <79056295+Ahmed-bahaa77@users.noreply.github.com>
2021-04-13 01:31:11 +0200

    inbox ,individualchat-network-loader-text from input-code refactor using listtile&others

--- lib/ui/inbox/inboxscreen_model.dart
+++ lib/ui/inbox/inboxscreen_model.dart
@@ -13,3 +13,9 @@ class ChatUsers {
     @required this.time,
   });
 }
+
+class ChatMessage {
+  String messageContent;
+  String messageType;
+  ChatMessage({@required this.messageContent, @required this.messageType});
+}
